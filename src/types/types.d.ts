db92b7f060a56bee48b7bf43b991ed4d482727d1
SeiwonPark <psw7347@gmail.com>
2023-08-04 17:32:39 +0900

    feat: add types for input data

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,6 +1,14 @@
 interface KeyValuePair {
   [key: string]: number | string
 }
+
+interface InputData {
+  [key: string]: {
+    level: number
+    data?: any
+  }
+}
+
 interface ThemeProps {
   level0: string
   level1: string
