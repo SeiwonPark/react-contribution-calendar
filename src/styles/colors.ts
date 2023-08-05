c426d7664aecee239d4c0ae81dda10e1fecffa62
yaehee <yaehee@kakao.com>
2023-08-05 18:30:35 +0900

    add theme colors

--- src/styles/colors.ts
+++ src/styles/colors.ts
@@ -1,4 +1,4 @@
-export const THEMES: Theme = {
+export const THEMES = {
   // 테마명 : LeveledColorProps 로 테마 컬러 지정
   grass: {
     level0: '#ebedf0',
@@ -7,18 +7,117 @@ export const THEMES: Theme = {
     level3: '#30a14e',
     level4: '#216e39',
   },
-  pink: {
+  cherry: {
     level0: '#fff0f6',
     level1: '#ffdeeb',
     level2: '#faa2c1',
     level3: '#f06595',
     level4: '#a61e4d',
   },
-  indigo: {
-    level0: '#edf2ff',
-    level1: '#bac8ff',
-    level2: '#748ffc',
-    level3: '#4263eb',
-    level4: '#364fc7',
+  cherry_blossom: {
+    level0: '#dedad2',
+    level1: '#e4bcad',
+    level2: '#df979e',
+    level3: '#d7658b',
+    level4: '#c80064',
+  },
+  pink: {
+    level0: '#ffeffc',
+    level1: '#ffc2f6',
+    level2: '#ffa4f1',
+    level3: '#ff86ed',
+    level4: '#ff68e9',
+  },
+  sky: {
+    level0: '#ffffff',
+    level1: '#badbdb',
+    level2: '#98d1d1',
+    level3: '#76c8c8',
+    level4: '#54bebe',
+  },
+  halloween: {
+    level0: '#ebedf0',
+    level1: '#ffee4a',
+    level2: '#ffc501',
+    level3: '#fe9600',
+    level4: '#03001c',
+  },
+  winter: {
+    level0: '#ebedf0',
+    level1: '#b6e3ff',
+    level2: '#54aeff',
+    level3: '#0969da',
+    level4: '#0a3069',
+  },
+  purquoise: {
+    level0: '#dee6ee',
+    level1: '#bdcddc',
+    level2: '#9db4cb',
+    level3: '#7c9bb9',
+    level4: '#5b82a8',
+  },
+  blue_pop: {
+    level0: '#ff008c',
+    level1: '#ff00e4',
+    level2: '#d400ff',
+    level3: '#9800ff',
+    level4: '#057dff',
+  },
+  mustard: {
+    level0: '#f5b40d',
+    level1: '#f7c33d',
+    level2: '#f9d26e',
+    level3: '#fbe19e',
+    level4: '#fdf0cf',
+  },
+  //dark themes
+  dark_vomit: {
+    level0: '#660099',
+    level1: '#663399',
+    level2: '#666699',
+    level3: '#669999',
+    level4: '#66cc99',
+  },
+  dark_pink: {
+    level0: '#000000',
+    level1: '#33142e',
+    level2: '#7f3474',
+    level3: '#cc53ba',
+    level4: '#ff68e9',
+  },
+  dark_blue: {
+    level0: '#330033',
+    level1: '#330066',
+    level2: '#330099',
+    level3: '#3300cc',
+    level4: '#3300ff',
+  },
+  dark_ocean: {
+    level0: '#0000b3',
+    level1: '#0010d9',
+    level2: '#0040ff',
+    level3: '#00bfff',
+    level4: '#00ffff',
+  },
+  dark_grass: {
+    level0: '#161b22',
+    level1: '#0e4429',
+    level2: '#006d32',
+    level3: '#26a641',
+    level4: '#39d353',
+  },
+  dark_halloween: {
+    level0: '#161b22',
+    level1: '#631c03',
+    level2: '#bd561d',
+    level3: '#fa7a18',
+    level4: '#fddf68',
+  },
+  dark_winter: {
+    level0: '#161b22',
+    level1: '#0a3069',
+    level2: '#0969da',
+    level3: '#54aeff',
+    level4: '#b6e3ff',
   },
 }
