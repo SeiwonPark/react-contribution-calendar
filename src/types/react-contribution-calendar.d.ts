7f81a161cc90b24f10fb93fc83aff7886542eaa5
SeiwonPark <psw7347@gmail.com>
2023-08-05 20:58:32 +0900

    docs: add docstring to types

--- src/types/react-contribution-calendar.d.ts
+++ src/types/react-contribution-calendar.d.ts
@@ -1,33 +1,24 @@
+// Type definitions for react-contribution-calendar 0.0.33
+// Project: https://github.com/seiwon-yaehee/react-contribution-calendar
+// Definitions by: Seiwon Park <https://github.com/SeiwonPark>
+//                 Yaehee Choe <https://github.com/YaeheeChoe>
+
+/// <reference path="./types.d.ts" />
+
 declare module 'react-contribution-calendar' {
   import { FunctionComponent } from 'react'
 
-  export interface KeyValuePair {
-    [key: string]: number | string
-  }
-
-  export interface InputDataProps {
-    level: number
-    data?: object
-  }
-
-  export interface InputData {
-    [key: string]: InputDataProps
-  }
-
-  export interface ThemeProps {
-    level0: string
-    level1: string
-    level2: string
-    level3: string
-    level4: string
-  }
-
-  export interface Theme {
-    [key: string]: ThemeProps
-  }
-
+  /**
+   * This is for attributes of <ContributionCalendar /> function component.
+   */
   export interface ContributionCalendarProps {
+    /**
+     * List of properties of each item in the data, defaults to an empty list `[]`.
+     */
     data: InputDataProps[]
+    /**
+     * The name of theme for the ContributionCalendar, defaults to `grass`.
+     */
     theme?: string
   }
 
