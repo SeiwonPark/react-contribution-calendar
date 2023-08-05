278558b4d5fe122f98a473d0cc8489aaff45d4d2
SeiwonPark <psw7347@gmail.com>
2023-08-05 10:07:56 +0900

    feat(npm): set configuration to deploy npm package

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,30 @@
+import { resolve } from 'path'
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import tsConfigPaths from 'vite-tsconfig-paths'
+import dts from 'vite-plugin-dts'
+import libCss from 'vite-plugin-libcss'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), dts(), tsConfigPaths(), libCss()],
+  build: {
+    lib: {
+      entry: resolve('src', 'components/index.ts'),
+      name: 'ContributionCalendar',
+      formats: ['es', 'umd'],
+      fileName: 'index',
+    },
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+        assetFileNames: 'styles/[name].[ext]',
+      },
+    },
+    emptyOutDir: true,
+  },
 })
