c40f16930a103390113a16ac3714d00da66049fb
SeiwonPark <psw7347@gmail.com>
2023-08-16 17:09:50 +0900

    fix: tooltip padding

--- src/components/Cell/index.tsx
+++ src/components/Cell/index.tsx
@@ -11,20 +11,54 @@ interface CellProps extends HTMLAttributes<HTMLTableCellElement> {
 
 export default function Cell({ children, style, dataTooltip, dataLevel, themeProps, ...otherProps }: CellProps) {
   const cellRef = useRef<HTMLTableCellElement>(null)
-  const [tooltipOffset, setTooltipOffset] = useState<number>(-30)
+  const [tooltipOffset, setTooltipOffset] = useState<number>(-10)
+
+  const getVisibleChildren = (parent: Node | null) => {
+    if (!parent) return 0
+
+    let count = 0
+    parent.childNodes.forEach((node: ChildNode) => {
+      if (node.nodeType === Node.ELEMENT_NODE) {
+        const style = window.getComputedStyle(node as Element)
+        if (style.display !== 'none') {
+          count++
+        }
+      }
+    })
+
+    return count
+  }
+
+  const getVisibleCellIndex = (cell: HTMLTableCellElement | null) => {
+    if (!cell || !cell.parentNode) return -1
+
+    const allCells: HTMLElement[] = Array.from(cell.parentNode.childNodes) as HTMLElement[]
+
+    let visibleIndex = 0
+
+    for (let i = 0; i < cell.cellIndex; i++) {
+      if (allCells[i].style.display !== 'none') {
+        visibleIndex++
+      }
+    }
+
+    return visibleIndex
+  }
 
   useEffect(() => {
     const handleMouseOver = () => {
       if (cellRef.current) {
-        const totalLength = cellRef.current.parentNode?.children.length
-        const cellIndex = cellRef.current.cellIndex
+        const totalLength = getVisibleChildren(cellRef.current?.parentNode)
+        const cellIndex = getVisibleCellIndex(cellRef.current)
 
         if (totalLength) {
-          let offset = -30
-          if (cellIndex / totalLength > 0.5) {
-            offset = Math.max(~~(cellRef.current.cellIndex * -200) / totalLength, -150)
+          let offset = -10
+          if (cellIndex / totalLength > 0.66) {
+            offset = Math.max(~~(cellRef.current.cellIndex * -150) / totalLength, -130)
+          } else if (cellIndex / totalLength > 0.33) {
+            offset = Math.max(~~(cellRef.current.cellIndex * -100) / totalLength, -120)
           } else {
-            offset = Math.min(~~(cellRef.current.cellIndex * -200) / totalLength, -30)
+            offset = Math.min(~~(cellRef.current.cellIndex * -50) / totalLength, -10)
           }
           setTooltipOffset(offset)
         }
