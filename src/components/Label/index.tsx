5a1e04f51d29b320c57d23e1cc85ac4ef3b60ec1
SeiwonPark <psw7347@gmail.com>
2023-08-21 14:42:49 +0900

    fix: remove unused attribute

--- src/components/Label/index.tsx
+++ src/components/Label/index.tsx
@@ -4,12 +4,11 @@ import './index.css'
 interface LabelProps extends HTMLAttributes<HTMLTableCellElement> {
   children: ReactNode
   textColor: string
-  cy?: number
   style?: CSSProperties
   colSpan?: number
 }
 
-export default function Label({ children, textColor, cy, style, colSpan, ...otherProps }: LabelProps) {
+export default function Label({ children, textColor, style, colSpan, ...otherProps }: LabelProps) {
   return (
     <td className="calendar-label" style={style} colSpan={colSpan} {...otherProps}>
       <span className="calendar-label-text" aria-hidden="true" style={{ color: textColor }}>
