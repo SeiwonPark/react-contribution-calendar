f72bb2e1aa9a8f7bd278984264a4c9f75e12d9a6
SeiwonPark <psw7347@gmail.com>
2023-08-27 12:41:08 +0900

    fix: description outline

--- src/components/Description/index.tsx
+++ src/components/Description/index.tsx
@@ -10,15 +10,37 @@ interface DescriptionProps {
 export default function Description({ textColor, cx, theme }: DescriptionProps) {
   const themeProps = createTheme(theme)
 
+  const isEmojiTheme = (theme: string | ThemeProps): boolean => {
+    return theme === 'emoji_positive' || theme === 'emoji_negative'
+  }
+
+  const cellStyle = (theme: string | ThemeProps, level: number) => {
+    if (isEmojiTheme(theme)) {
+      return { fontSize: cx }
+    } else {
+      return { backgroundColor: themeProps[`level${level}`], outline: '1px solid #1b1f230f' }
+    }
+  }
+
   return (
     <div className="description">
       <div className="themes">
         <span style={{ color: textColor, fontSize: cx }}>Less</span>
-        <div className="cell" style={{ backgroundColor: themeProps.level0 }} />
-        <div className="cell" style={{ backgroundColor: themeProps.level1 }} />
-        <div className="cell" style={{ backgroundColor: themeProps.level2 }} />
-        <div className="cell" style={{ backgroundColor: themeProps.level3 }} />
-        <div className="cell" style={{ backgroundColor: themeProps.level4 }} />
+        <div className="cell" style={{ ...cellStyle(theme, 0) }}>
+          {isEmojiTheme(theme) ? themeProps.level0 : undefined}
+        </div>
+        <div className="cell" style={{ ...cellStyle(theme, 1) }}>
+          {isEmojiTheme(theme) ? themeProps.level1 : undefined}
+        </div>
+        <div className="cell" style={{ ...cellStyle(theme, 2) }}>
+          {isEmojiTheme(theme) ? themeProps.level2 : undefined}
+        </div>
+        <div className="cell" style={{ ...cellStyle(theme, 3) }}>
+          {isEmojiTheme(theme) ? themeProps.level3 : undefined}
+        </div>
+        <div className="cell" style={{ ...cellStyle(theme, 4) }}>
+          {isEmojiTheme(theme) ? themeProps.level4 : undefined}
+        </div>
         <span style={{ color: textColor, fontSize: cx }}>More</span>
       </div>
     </div>
