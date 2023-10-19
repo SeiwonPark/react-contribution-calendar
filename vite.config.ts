b80e4f33aa2dd1f5be179c7a8e22427e9f6bf0a9
Simon Laux <Simon-Laux@users.noreply.github.com>
2023-10-19 01:45:53 +0000

    fix: exclude `react/jsx-runtime` from bundle
    before:
    dist/index.js          39.18 kB │ gzip: 11.81 kB
    dist/index.umd.cjs  26.25 kB │ gzip: 10.26 kB
    
    after:
    dist/index.js          18.00 kB │ gzip: 5.68 kB
    dist/index.umd.cjs  12.70 kB │ gzip: 5.10 kB

--- vite.config.ts
+++ vite.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
       fileName: 'index',
     },
     rollupOptions: {
-      external: ['react', 'react-dom'],
+      external: ['react', 'react-dom', 'react/jsx-runtime'],
       output: {
         globals: {
           react: 'React',
